aa9a1b289902bd7f8f5372d2f71cbd05c5d43d4b
Tony <Tony@tangyangjindeMacBook-Pro.local>
2015-09-09 16:08:29 +0800

    APP导航栏新增

---
+++ GrabTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrabTask
+//
+//  Created by John on 15/9/8.
+//  Copyright (c) 2015年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
